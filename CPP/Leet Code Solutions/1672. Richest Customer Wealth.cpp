b7fd217cc3398a9fc3f6f30bd0365c049b4c21df
K-Ananya27 <60320929+K-Ananya27@users.noreply.github.com>
2022-10-31 22:34:51 +0530

    Create 1672. Richest Customer Wealth.cpp

---
+++ CPP/Leet Code Solutions/1672. Richest Customer Wealth.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max=0;
+        int total=0;
+        for(int i=0;i<accounts.size();i++)
+        {
+            total=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                total+=accounts[i][j];
+            }
+            if(total>max)
+            {
+                max=total;
+            }
+        }
+        return max;
+    }
+};
